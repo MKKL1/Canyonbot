453a3e3eb234dd0ef16546b1940f06d0c83fa0e9
MKKL1 <Mkkl3301@gmail.com>
2023-10-25 16:26:13 +0200

    Replace route prefix only if query starts with it

--- src/main/java/com/mkkl/canyonbot/music/search/internal/sources/youtube/YoutubeLinkSearch.java
+++ src/main/java/com/mkkl/canyonbot/music/search/internal/sources/youtube/YoutubeLinkSearch.java
@@ -17,10 +17,14 @@ public class YoutubeLinkSearch implements SearchSource {
 
     @Override
     public AudioItem search(String query) {
-        return sourceManager.loadItem(null, new AudioReference(
-                query
-                        .replace(YoutubeMusicSearch.route, "")
-                        .replace(YoutubeSearch.route, ""),
-                null));
+        return sourceManager.loadItem(null, new AudioReference(stripRoutePrefixes(query) ,null));
+    }
+
+    private String stripRoutePrefixes(String query) {
+        if(query.startsWith(YoutubeMusicSearch.route))
+            query = query.substring(YoutubeMusicSearch.route.length());
+        if(query.startsWith(YoutubeSearch.route))
+            query = query.substring(YoutubeSearch.route.length());
+        return query;
     }
 }
